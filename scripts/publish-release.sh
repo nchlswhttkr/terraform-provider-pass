264c3b0be3f290c08a1d2f3c345dfccd0ceebfc9
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-09-24 18:55:40 +1000

    Fix release script, support prereleases

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 function get_latest_build_number_for_tag () {
     # $1 - The tag name
-    curl --silent --fail --show-error "https://api.buildkite.com/v2/organizations/nchlswhttkr/pipelines/terraform-provider-pass/builds?branch=v${1}&per_page=1" -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" \
+    curl --silent --fail --show-error "https://api.buildkite.com/v2/organizations/nchlswhttkr/pipelines/terraform-provider-pass/builds?branch=${1}&per_page=1" -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" \
         | jq --raw-output ".[0].number"
 }
 
@@ -44,12 +44,12 @@ function download_artifact_from_build () {
     curl --silent --fail --show-error "${download_url}" > "${1}"
 }
 
-PREVIOUS_RELEASE_TAG="$(git describe --abbrev=0)"
-read -rp "Enter release version (previously ${PREVIOUS_RELEASE_TAG}) > v" RELEASE;
+PREVIOUS_RELEASE_TAG="$(git describe --abbrev=0 --exclude='*-*')"
+read -rp "Enter release version (found ${PREVIOUS_RELEASE_TAG} previously) > v" RELEASE;
 mkdir -p "release/${RELEASE}"
 
 # Ensure version number is valid
-if ! [[ "${RELEASE}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if ! [[ "${RELEASE}" =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(-[[:alnum:]]+)?$ ]]; then
     echo -e "\033[31mRelease version number does not satisfy pattern\033[0m"
     exit 1
 fi
@@ -58,12 +58,11 @@ if grep --fixed-strings --line-regexp "v${RELEASE}" <(git tag --list) > /dev/nul
 else
     echo "--- Creating and pushing tag for release"
     TAG_MESSAGE=$(mktemp)
-    git log --format="%s" "${PREVIOUS_RELEASE_TAG}..main" \
+    git log --format="%s" "${PREVIOUS_RELEASE_TAG}..HEAD" \
         | sed "s/^/* /" \
         | cat <(echo "v${RELEASE}") <(echo "") - > "${TAG_MESSAGE}"
-    git tag --file="${TAG_MESSAGE}" --edit "v${RELEASE}" "main"
-    git push origin main
-    git push --tags
+    git tag --file="${TAG_MESSAGE}" --edit "v${RELEASE}"
+    git push --tags origin HEAD
 fi
 
 echo "--- Fetching latest build for v${RELEASE} on Buildkite"
@@ -72,8 +71,9 @@ BUILDKITE_BUILD_NUMBER=$(get_latest_build_number_for_tag "v${RELEASE}")
 wait_for_build_to_pass "${BUILDKITE_BUILD_NUMBER}"
 
 echo "--- Crafting release v${RELEASE}"
-download_artifact_from_build "release.zip" "${BUILDKITE_BUILD_NUMBER}"
-unzip "release.zip" -d "release/${RELEASE}"
+download_artifact_from_build release.zip "${BUILDKITE_BUILD_NUMBER}"
+unzip release.zip -d "release/${RELEASE}"
+rm release.zip
 
 echo "--- Generate signed checksums"
 cd "release/${RELEASE}"
@@ -83,12 +83,18 @@ cd ../..
 
 echo "--- Creating a draft release"
 GITHUB_ACCESS_TOKEN=$(pass show terraform-provider-pass/github-access-token)
+PRERELEASE=false
+if [[ "${RELEASE}" =~ - ]]; then
+    echo -e "\033[33mThis release will be marked as a prerelease\033[0m"
+    PRERELEASE=true
+fi
 curl --silent --fail --show-error -X POST "https://api.github.com/repos/nchlswhttkr/terraform-provider-pass/releases" \
     -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
     -H "Accept: application/vnd.github.v3+json" \
     --data "
         {
             \"tag_name\": \"v${RELEASE}\",
+            \"prerelease\": ${PRERELEASE},
             \"draft\": true
         }
     " > "release/${RELEASE}.json"
